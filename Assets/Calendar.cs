60efac0d9e805cedf4557c44ed4130804681dad7
Scout <scout@miller5.us>
2023-11-13 23:30:31 -0800

    basic scene setup

--- Assets/Calendar.cs
+++ Assets/Calendar.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Calendar : MonoBehaviour
 {
+    [SerializeField] private int targetScene;
     public void Activate()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene(targetScene);
     }
 }
+
