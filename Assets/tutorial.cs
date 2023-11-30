3fa71bcf3fed595660d0847fc67abb95eeefc313
Scout <tuocs2001@gmail.com>
2023-11-29 21:31:09 -0800

    some tutorial audio

--- Assets/tutorial.cs
+++ Assets/tutorial.cs
@@ -7,12 +7,15 @@ public class tutorial : MonoBehaviour
 {
     [SerializeField] private TMP_Text text;
     [SerializeField] private GameObject Cup;
+    [SerializeField] private AudioSource Audio;
+
     private int phase = 1;
     public void PhaseTwo()
     {
         if (phase != 1) { return; }
         phase = 2;
-        text.text = "Now activate the walet by pressing the trigger to drop money on the counter for a coffee";
+        text.text = "Now activate the wallet by pressing the trigger to drop money on the counter for a coffee";
+        Audio.Play();
     }
     public void PhaseThree()
     {
@@ -20,5 +23,6 @@ public class tutorial : MonoBehaviour
         phase = 3;
         Cup.SetActive(true);
         text.text = "Now grab and enjoy your coffee, be carefull not to drop it carelessly";
+        Audio.Play();
     }
 }
