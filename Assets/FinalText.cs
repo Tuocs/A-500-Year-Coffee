783b05450bfcad13550e40218cfb58ef7ee18f3a
Scout <tuocs2001@gmail.com>
2023-12-13 21:36:12 -0800

    sfghjxfgj

--- Assets/FinalText.cs
+++ Assets/FinalText.cs
@@ -7,8 +7,17 @@ public class FinalText : MonoBehaviour
 {
     public TMP_Text text;
     // Update is called once per frame
+
+    private void Start()
+    {
+        text.text = "you have been playing this game for\n" + 
+            Mathf.RoundToInt(Time.time - GameManager.startTime + 120) + 
+            "\nSeconds\n " +
+            "but if your cup was paper it would have only lasted " + 
+            (Time.time - GameManager.startTime + 120);
+    }
     void Update()
     {
-        text.text = "it has been\n" + Mathf.RoundToInt(Time.time - GameManager.startTime) + "\nSeconds";
+        //text.text = "you have been playing this game for\n" + Mathf.RoundToInt(Time.time - GameManager.startTime) + "\nSeconds\n but if your cup was paper it would have only lasted " + Mathf.Round(Time.time - GameManager.startTime);
     }
 }
